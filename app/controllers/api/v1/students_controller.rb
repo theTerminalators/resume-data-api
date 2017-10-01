fcf715f744be6cd865a2d5b83dc010d39d062a68
alianam <aliana@betterment.com>
2017-10-01 14:38:45 -0400

    Updates to controller

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -11,19 +11,38 @@ class Api::V1::StudentsController < ApplicationController
 
   def create
     @new_student = Student.new(
-    first_name: params["first_name"],
-    last_name: params["last_name"],
-    email: params["email"],
-    phone_number: params["phone_number"],
-    short_bio: params["short_bio"],
-    linkedin_url: params["linkedin_url"],
-    twitter_handle: params["twitter_handle"],
-    personal_site: params["personal_site"],
-    online_resume: params["online_resume"],
-    github_url: params["github_url"],
-    photo: params["photo"]
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"],
+      phone_number: params["phone_number"],
+      short_bio: params["short_bio"],
+      linkedin_url: params["linkedin_url"],
+      twitter_handle: params["twitter_handle"],
+      personal_site: params["personal_site"],
+      online_resume: params["online_resume"],
+      github_url: params["github_url"],
+      photo: params["photo"]
     )
     @new_student.save
+    p params
+    redirect_to '/students'
+  end
+
+  def update
+    @edit_student = Student.find_by(id: params[:id])
+    @edit_student.update(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      email: params["email"],
+      phone_number: params["phone_number"],
+      short_bio: params["short_bio"],
+      linkedin_url: params["linkedin_url"],
+      twitter_handle: params["twitter_handle"],
+      personal_site: params["personal_site"],
+      online_resume: params["online_resume"],
+      github_url: params["github_url"],
+      photo: params["photo"]
+    )
     redirect_to '/students'
   end
 end
