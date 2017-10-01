d4c589e5bc57e76787ee742025cfb107a5959077
alianam <aliana@betterment.com>
2017-10-01 11:35:23 -0400

    Add two student rows

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Aliana", last_name: "Melendez", email: "aliana@betterment.com", phone_number: "555-555-5555", short_bio: "There is some bio infomation here", linkedin_url: "www.linkedin.com", twitter_handle: "twitter", personal_site: "www.mysite.whatever", online_resume: "whatever.pdf", github_url: "www.github.com/alianam", photo: "url.goes.here")
+
+Student.create(first_name: "Joe", last_name: "Joseph", email: "joe@joe.com", phone_number: "555-555-5555", short_bio: "There is some bio infomation here", linkedin_url: "www.linkedin.com", twitter_handle: "twitter", personal_site: "www.mysite.whatever", online_resume: "whatever.pdf", github_url: "www.github.com/alianam", photo: "url.goes.here")
